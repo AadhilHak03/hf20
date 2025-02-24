a6a7dceeae98af51580e3117c1f45c6faf2dc9fd
aadhi <aadhilhakeem03@gmail.com>
2025-02-24 01:54:07 -0500

    webhook 1

---
+++ src/test/java/MainTests/WEBHOOK1.java
@@ -0,0 +1,12 @@
+package MainTests;
+
+import org.testng.annotations.Test;
+
+public class WEBHOOK1 {
+	@Test
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing 1");
+	}
+
+}

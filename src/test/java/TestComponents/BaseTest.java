3adcd3f5e5875bfe1816ee0bd4256ed7b0f851f1
aadhi <aadhilhakeem03@gmail.com>
2025-02-24 22:38:27 -0500

    Browser Parameterized

--- src/test/java/TestComponents/BaseTest.java
+++ src/test/java/TestComponents/BaseTest.java
@@ -1,7 +1,14 @@
 package TestComponents;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeMethod;
 
 import PageObjects.landingPage;
@@ -11,13 +18,29 @@ public class BaseTest {
 	public WebDriver dr;
 	public landingPage lp;
 	
-	public WebDriver startDriver() {
+	public WebDriver startDriver() throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/GlobalData/BrowserSelection.properties");
+		prop.load(fis);
+		//String browser = "FireFox";
+		
+		String browserName= System.getProperty("browserName");
+		
+		if(browserName.contains("Chrome")) {
 		dr = new ChromeDriver();
+		}
+		else if (browserName.equalsIgnoreCase("FireFox")) {
+			dr = new FirefoxDriver();
+		}
+		else if (browserName.equalsIgnoreCase("Edge")) {
+			dr = new EdgeDriver();
+		}
+		dr.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
 		return dr;
 	}
 	
-	@BeforeMethod
-	public landingPage getPage()
+	@BeforeMethod(alwaysRun=true)
+	public landingPage getPage() throws IOException
 	{
 		dr = startDriver();
 		lp = new landingPage(dr);

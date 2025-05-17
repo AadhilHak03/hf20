a00f9c168fdb496edea1f724f2bcefbaec268e24
Aadhil New <aadhilhakeem03@gmail.com>
2025-05-17 12:18:43 -0400

    webhoob pt1

--- src/test/java/MainTests/WEBHOOK1.java
+++ src/test/java/MainTests/WEBHOOK1.java
@@ -3,7 +3,7 @@ package MainTests;
 import org.testng.annotations.Test;
 
 public class WEBHOOK1 {
-	@Test
+	/*@Test
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing 1");
@@ -12,6 +12,6 @@ public class WEBHOOK1 {
 	@Test
 	public void test1() {
 		System.out.println("Test1");
-	}
+	}*/
 	
 }
